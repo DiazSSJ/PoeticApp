52c4eac14716903595dc0fabfcee1a3b4637b538
NataliaLopezO <natalia.lopez.osorio@correounivalle.edu.co>
2024-03-26 16:08:50 -0500

    grabar audio y conexion con api ai speech azure

--- public/app.js
+++ public/app.js
@@ -2,6 +2,11 @@ const button = document.querySelector("#generate");
 const output = document.querySelector("#output");
 const descargaButton = document.querySelector(".descarga");
 
+//grabar audios
+const recordAnimo = document.getElementById("recordAnimo");
+let mediaRecorder;
+let audioChunks = [];
+
 
 const server_url = '/generate' 
 
@@ -41,3 +46,72 @@ descargaButton.addEventListener("click", async () => {
 
   html2pdf().set(opt).from(output).save();
 });
+
+
+//grabar audios
+
+async function startRecording() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+
+    mediaRecorder = new MediaRecorder(stream);
+
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        audioChunks.push(event.data);
+      }
+    };
+
+    mediaRecorder.onstop = () => {
+      //clearInterval(timerInterval);
+      const audioBlob = new Blob(audioChunks, {
+        type: 'audio/wav; codecs=opus',
+      });
+
+      // Envía el audio grabado al servidor
+      const formData = new FormData();
+      formData.append("audio", audioBlob);
+
+      const response = fetch('/upload-audio', {
+        method: 'POST',
+        body: formData
+      });
+    
+      
+    };
+
+    mediaRecorder.start();
+    console.log("grabando")
+    //startTime = Date.now();
+    //timerInterval = setInterval(updateRecordingTime, 1000);
+    recordAnimo.classList.remove("bi-mic-fill");
+    recordAnimo.classList.add("text-danger", "bi-mic-mute-fill");
+    recordAnimo.classList.add("recording"); // Agregar la clase "recording"
+  } catch (error) {
+    console.error("Error al acceder al micrófono:", error);
+  }
+}
+
+function stopRecording() {
+  if (mediaRecorder && mediaRecorder.state === "recording") {
+    //clearInterval(timerInterval);
+    mediaRecorder.stop();
+    recordAnimo.classList.remove("text-danger", "bi-mic-mute-fill");
+    recordAnimo.classList.add("bi-mic-fill");
+    console.log("no grabando")
+    recordAnimo.classList.remove("recording");
+
+  }
+}
+
+recordAnimo.addEventListener("click", () => {
+  if (mediaRecorder && mediaRecorder.state === "recording") {
+    stopRecording();
+  } else {
+    startRecording();
+  }
+});
+
+//grabar audio
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import fetch from 'node-fetch'; 
-import express from 'express';
+import express, { response } from 'express';
 import cors from 'cors';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -18,6 +21,12 @@ const port = 3000;
 const host = '0.0.0.0'
 
 const API_KEY = process.env.API_KEY;
+const AZURE_KEY = process.env.AZURE_KEY;
+
+// Configurar multer para manejar la carga de archivos de audio
+const upload = multer({
+  dest: 'uploads/' // Directorio donde se guardarán los archivos
+});
 
 async function getCompletion(messages) {
   const response = await fetch('https://api.openai.com/v1/chat/completions', {
@@ -34,6 +43,19 @@ async function getCompletion(messages) {
   });
 
   return response;
+}
+
+function getSpeechToText(messages) {
+  const response = fetch('https://eastus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=es-ES', {
+    method: "POST",
+    headers: {
+      "Content-Type": "audio/wav",
+      "Ocp-Apim-Subscription-Key": `${AZURE_KEY}`,
+    },
+    body: messages
+  });
+
+  return response
 }
 
 app.use(express.static('public'));
@@ -68,6 +90,44 @@ app.post('/generate', async (req, res) => {
   }
 
 });
+
+//grabar audio
+// Ruta para manejar la carga de archivos de audio
+app.post('/upload-audio', upload.single('audio'), async (req, res) => {
+ 
+  if (!req.file) {
+    return res.status(400).send("No se proporcionó ningún archivo de audio.");
+  }
+  //console.log(req.file)
+
+  const response = await getSpeechToText(req.file);
+  response.json()
+  .then(data => {
+    console.log(data); // Aquí puedes acceder a los datos JSON
+  })
+  .catch(error => {
+    console.error("Error al obtener los datos JSON:", error);
+  });
+
+  const targetDir = 'uploads/';
+
+  const randomFileName = `${Date.now()}_${Math.floor(Math.random() * 1000)}.opus`;
+  const targetFile = path.join(targetDir, randomFileName);
+
+  fs.rename(req.file.path, targetFile, (err) => {
+    if (err) {
+      return res.status(500).send('Error al guardar el archivo de audio.');
+    }
+    res.send(`Audio guardado exitosamente con el nombre: ${randomFileName}`);
+  });
+
+  //console.log(response.body);
+
+});
+
+
+
+//grabar audio
 
 app.listen(port, host, () => {
   console.log(`Server is running at http://localhost:${port}`);
